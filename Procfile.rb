f491b148c3d3bf45749462611fa4cf52b869da66
kingsley <kingsleychi@gmail.com>
2015-06-21 22:44:48 -0400

    init

--- Procfile.rb
+++ Procfile.rb
@@ -1,2 +1,3 @@
-clock: bundle exec clockwork clock.rb
+#clock: bundle exec clockwork clock.rb
 worker: bundle exec sidekiq -r ./app/model/feed_worker.rb -c 5
+web: bundle exec rails s -p ${PORT:-3000} -e ${RACK_ENV:-production}
