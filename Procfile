d9ba96f929bd3b99bb491a7d40ac1b010127961c
kingsley <kingsleychi@gmail.com>
2015-06-25 19:57:04 -0400

    feed

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-worker: bundle exec sidekiq -r ./app/model/feed_worker.rb -c 5
+worker: bundle exec sidekiq -r app/model/feed_worker.rb -c 5
 web: bundle exec rails s -p ${PORT:-3000} -e ${RACK_ENV:-production}
--- app/models/feed_worker.rb
+++ app/models/feed_worker.rb
@@ -18,29 +18,31 @@ class FeedWorker
   include Sidekiq::Worker
 
   def perform()
-    require 'open-uri'
-    last_update = Misc.find_by_name "reddit_last_update"
-    last_updated = last_update.value
+	last_update = Misc.find_by_name "reddit_last_update"
+	last_updated = last_update.value
+	  puts last_update.value
+	@url = "http://www.reddit.com/r/UpliftingNews/new.json"
+	@doc = JSON.parse(open(@url).read)
+	@doc['data']['children'].each_with_index do |data, index|
+	  @last_feed_time = data['data']['created'] if index == 0
+	  p = Post.new
+	  next if data['data']['preview'].nil?
+	  break if last_updated >= data['data']['created']
 
-    @url = "http://www.reddit.com/r/UpliftingNews/new.json"
-    @doc = JSON.parse(open(@url).read)
-    @doc['data']['children'].each_with_index do |data, index|
-    	last_feed_time = data['data']['created'] if index == 0
-    	p = Post.new
-    	next if data['data']['preview'].nil?
-    	break if last_updated >= data['data']['created']
+	  p.external_image_url = data['data']['preview']['images'][0]['source']['url']
+	  p.title = data['data']['title']
+	  p.url = data['data']['url']
+	  p.created_at = Time.zone.at data['data']['created_utc']
+	  p.post_type = Post::PTYPE[:reddit]
+	  p.external_author = data['data']['author']
+	  p.external_author_url = "http://www.reddit.com/user/#{data['data']['author']}"
+	  p.external = true
+	  p.save
+	  puts "feed"
 
-    	p.image = data['data']['preview']['images'][0]['source']['url']
-    	p.title = data['data']['title']
-    	p.url = data['data']['url']
-    	p.created_at = data['data']['created']
-    	p.post_type = Post::PTYPE[:reddit]
-    	p.external = true
-    	p.save
-
-    end
-    last_update.value = last_feed_time
-    last_update.save
+	end
+	last_update.value = @last_feed_time
+	last_update.save
 
   end
 end
