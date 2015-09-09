26c5391ad4ad277ce87a4475a9b5d80b179c92ed
Umer Bilal <omerpucit@gmail.com>
2015-09-09 22:14:08 +0500

    added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,7 +24,23 @@ module ApplicationHelper
 
   def default_e_image(post)
     if post.external_image_url.present?
-
+      return post.external_image_url
+    else
+      rand_n = rand(1..6)
+      case rand_n
+      when 1
+        return 'crane-default.jpg'
+      when 2
+        return 'purple.jpg'
+      when 3
+        return 'pink.jpg'
+      when 4
+        return 'orange.jpg'
+      when 5
+        return 'green.jpg'
+      when 6
+        return 'blue.jpg'
+      end
     end
   end
 
